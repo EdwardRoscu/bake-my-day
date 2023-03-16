74c47563873fbb9b3693ca5ca6e3e894f867f0ea
Dana M <dana.marinescu01@yahoo.com>
2023-03-16 12:59:07 +0200

    Created the Navbar.jsx file

---
+++ client/src/state/index.js
@@ -0,0 +1,17 @@
+/*import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isCartOpen: false,
+    cart: [],
+    items: [],
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        setItems: (state, action) => {
+            state.items = action.payload;
+        }
+    }
+})*/
