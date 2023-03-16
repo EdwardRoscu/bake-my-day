913cb8b6dda37f1b3d835ca8daeba8053e08588d
Dana M <dana.marinescu01@yahoo.com>
2023-03-16 12:59:07 +0200

    Created the Navbar.jsx file

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import {
   Route,
   useLocation
 } from 'react-router-dom';
+import Checkout from "./scenes/checkout/Checkout";
 import Home from "./scenes/home/Home";
-
+import ItemDetails from "./scenes/itemDetails/itemDetails";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 const ScrollToTop = () => {
   const {pathname } = useLocation();
 
@@ -20,9 +23,13 @@ const ScrollToTop = () => {
 function App() {
   return <div className="App">
     <BrowserRouter>
+    <Navbar />
      <ScrollToTop />
      <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="item/:itemId" element={<ItemDetails />} />
+      <Route path="checkout" element={<Checkout />} />
+      <Route path="checkout/success" element={<Confirmation />} />
      </Routes>
     </BrowserRouter>
   </div>;
