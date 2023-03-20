67be16a76d32493d78491777bf77c0f835b4fde2
Edward Roșcu <edward.roscu@yahoo.com>
2023-03-21 01:44:24 +0200

    Finished the cart icon and the cart menu.

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,10 +1,12 @@
-/*import { createSlice } from '@reduxjs/toolkit';
+// Cart behaviour
+
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     isCartOpen: false,
     cart: [],
-    items: [],
-}
+    items: []
+};
 
 export const cartSlice = createSlice({
     name: "cart",
@@ -12,6 +14,47 @@ export const cartSlice = createSlice({
     reducers: {
         setItems: (state, action) => {
             state.items = action.payload;
+        },
+
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item];
+        },
+
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+        },
+
+        increaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.id) {
+                    item.count++;
+                }
+                return item;
+            });
+        },
+
+        decreaseCount: (state, action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.id && item.count > 1) {
+                    item.count--;
+                }
+                return item;
+            });
+        },
+
+        setIsCartOpen: (state) => {
+            state.isCartOpen = !state.isCartOpen;
         }
     }
-})*/
+});
+
+export const {
+    setItems,
+    addToCart,
+    removeFromCart,
+    increaseCount,
+    decreaseCount,
+    setIsCartOpen
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
