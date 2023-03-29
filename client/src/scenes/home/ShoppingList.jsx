d3ef6b5e1665d57c2237126b863519471d6aaa58
Edward Roșcu <edward.roscu@yahoo.com>
2023-03-30 00:53:17 +0300

    Finished the ShoppingList

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -6,13 +6,13 @@ import Item from "../../components/Item";
 import { useMediaQuery } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { setItems } from '../../state';
+import { Typography } from "@mui/material";
 
 const ShoppingList = () => {
     const dispatch = useDispatch();
     const [value, setValue] = useState("all");
     const items = useSelector((state) => state.cart.items);
-    const isNonMobile = useMediaQuery("(min-width:600px)");
-    console.log("items", items);
+    const breakPoint = useMediaQuery("(min-width:600px)");
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -21,7 +21,7 @@ const ShoppingList = () => {
     async function getItems() {
         const items = await fetch(
             "http://localhost:1337/api/items?populate=image",
-            {method: "GET"}
+            { method: "GET" }
         );
         const itemsJson = await items.json();
         dispatch(setItems(itemsJson.data));
@@ -31,8 +31,66 @@ const ShoppingList = () => {
         getItems();
     }, []);
 
+    const cakes = items.filter(
+        (item) => item.attributes.category === "cakes"
+    );
+    const cookies = items.filter(
+        (item) => item.attributes.category === "cookies"
+    );
+    const pies = items.filter(
+        (item) => item.attributes.category === "pies"
+    );
+
     return (
-        <div>Shopping List</div>
+        <Box width="80%" margin="80px auto">
+            <Typography variant="h3" textAlign="center">
+                Featured <b>Products</b>
+            </Typography>
+            <Tabs
+                textColor="primary"
+                indicatorColor="primary"
+                value={value}
+                onChange={handleChange}
+                centered
+                TabIndicatorProps={{ sx: { display: breakPoint ? "block" : "none" } }}
+                sx={{
+                    m: "25px",
+                    "& .MuiTabs-flexContainer": {
+                        flexWrap: "wrap",
+                    },
+                }}
+            >
+                <Tab label="ALL" value="all" />
+                <Tab label="CAKES" value="cakes" />
+                <Tab label="COOKIES" value="cookies" />
+                <Tab label="PIES" value="pies" />
+            </Tabs>
+            <Box
+                margin="0 auto"
+                display="grid"
+                gridTemplateColumns="repeat(auto-fill, 300px)"
+                justifyContent="space-around"
+                rowGap="20px"
+                columnGap="1.5%"
+            >
+                {value === "all" &&
+                    items.map((item) => (
+                        <Item item={item} key={`${item.name}-${item.id}`} />
+                    ))}
+                {value === "cakes" &&
+                    cakes.map((item) => (
+                        <Item item={item} key={`${item.name}-${item.id}`} />
+                    ))}
+                {value === "cookies" &&
+                    cookies.map((item) => (
+                        <Item item={item} key={`${item.name}-${item.id}`} />
+                    ))}
+                {value === "pies" &&
+                    pies.map((item) => (
+                        <Item item={item} key={`${item.name}-${item.id}`} />
+                    ))}
+            </Box>
+        </Box>
     );
 };
 
