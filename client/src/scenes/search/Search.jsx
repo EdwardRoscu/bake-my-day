0a459446532923ea086cdb093f3e11d5cff842ec
Dana Marinescu <dana.marinescu@student.upt.ro>
2023-05-09 18:31:25 +0300

    An intermediate state for Search page
    the products must be displayed as on the home page

--- client/src/scenes/search/Search.jsx
+++ client/src/scenes/search/Search.jsx
@@ -1,21 +1,13 @@
-import { Box, Button, Container, TextField, IconButton, Typography } from "@mui/material";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import { Box, Container, Input, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Item from "../../components/Item";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import { shades } from "../../theme";
-import { addToCart } from "../../state";
 import { useDispatch } from "react-redux";
-import {SearchOutlined} from "@mui/icons-material";
+
 const Search = ({}) => {
     const dispatch = useDispatch();
     const { itemId } = useParams();
-    const [value, setValue] = useState('');
-    const [count, setCount] = useState(1);
+    const [value, setValue] = useState("");
     const [item, setItem] = useState(null);
     const [items, setItems] = useState([]);
 
@@ -48,104 +40,47 @@ const Search = ({}) => {
     }, [itemId]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
+      
         <Box width="80%" m="100px auto">
           <Container maxWidth="s">
           <Typography variant="h5">Searching for the Best Dessert &#x1F50E;</Typography>
-    
-          <form onSubmit={handleChange}>
-              <TextField
+          
+            <Box>
+              <Input
                 fullWidth
-                onChange={handleChange}
+                onChange={(event) => {setValue(event.target.value);}}
                 margin="normal" 
                 type="text"
                 id="search-field"
                 className="form-control"
                 placeholder="What is your sweet desire?! SEARCH HERE!"
-                />
-                
-          </form>
+              />
+            </Box>
+            <Box>
+               {
+                item?.filter((searched) => {
+                  if(value == ""){
+                     return searched;
+                  } else if(searched.name.toLowerCase().includes(value.toLowerCase())){
+                     return searched;
+                  }
+                })
+                .map((searched) => {
+                  return(
+                  <Box
+                    mt="20px"
+                    display="flex"
+                    flexWrap="wrap"
+                    columnGap="1.33%"
+                    justifyContent="space-between"
+                    >
+                     <Item key={`${searched.name}`} item={searched} />
+                  </Box>
+                  )
+                })
+               } 
+             </Box>
           </Container>
-          {/*  <Box display="flex" flexWrap="wrap" columnGap="40px">
-                {/* IMAGES 
-                <Box flex="1 1 40%" mb="40px">
-                    <img
-                        alt={item?.name}
-                        width="100%"
-                        height="100%"
-                        src={`http://localhost:4000${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
-                        style={{ objectFit: "contain" }}
-                    />
-                </Box>
-
-                {/* ACTIONS 
-                <Box flex="1 1 50%" mb="40px">
-                    <Box display="flex" justifyContent="space-between">
-                        <Box>Home/Item</Box>
-                        <Box>Prev Next</Box>
-                    </Box>
-
-                    <Box m="65px 0 25px 0">
-                        <Typography variant="h3">{item?.attributes?.name}</Typography>
-                        <Typography>${item?.attributes?.price}</Typography>
-                        <Typography sx={{ mt: "20px" }}>
-                            {item?.attributes?.longDescription}
-                        </Typography>
-                    </Box>
-
-                    <Box display="flex" alignItems="center" minHeight="50px">
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            border={`1.5px solid ${shades.neutral[300]}`}
-                            mr="20px"
-                            p="2px 5px"
-                        >
-                            <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
-                                <RemoveIcon />
-                            </IconButton>
-                            <Typography sx={{ p: "0 5px" }}>{count}</Typography>
-                            <IconButton onClick={() => setCount(Math.min(count + 1, 50))}>
-                                <AddIcon />
-                            </IconButton>
-                        </Box>
-                        <Button
-                            sx={{
-                                backgroundColor: "#222222",
-                                color: "white",
-                                borderRadius: 0,
-                                minWidth: "150px",
-                                padding: "10px 40px",
-                            }}
-                            onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
-                        >
-                            ADD TO CART
-                        </Button>
-                    </Box>
-                    <Box>
-                        <Box m="20px 0 5px 0" display="flex">
-                            <FavoriteBorderOutlinedIcon />
-                            <Typography sx={{ ml: "5px" }}>ADD TO WISHLIST</Typography>
-                        </Box>
-                        <Typography>CATEGORIES: {item?.attributes?.category}</Typography>
-                    </Box>
-                </Box>
-            </Box>
-
-            {/* INFORMATION 
-            <Box m="20px 0">
-                <Tabs value={value} onChange={handleChange}>
-                    <Tab label="DESCRIPTION" value="description" />
-                    <Tab label="REVIEWS" value="reviews" />
-                </Tabs>
-            </Box>
-            <Box display="flex" flexWrap="wrap" gap="15px">
-                {value === "description" && (
-                    <div>{item?.attributes?.longDescription}</div>
-                )}
-                {value === "reviews" && <div>reviews</div>}
-            </Box> */}
-
-            {/* RELATED ITEMS */}
             <Box mt="50px" width="100%">
                 <Typography variant="h3" fontWeight="bold">
                     Related Products
